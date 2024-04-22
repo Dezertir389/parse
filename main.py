370d6fe63ef4d03f8fa7c0d595d2d686118254cf
lacozavr <151935499+lacozavr@users.noreply.github.com>
2024-04-22 19:14:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from data import db_session
 from data.users import User
+from data.flowers import Flowers
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -11,38 +13,25 @@ def log():
     return render_template('sign_in.html')
 
 
-@app.route('/sign_up')
-def log_ib():
-    return render_template('sign_up.html')
-
-
-@app.route('/register', methods=['POST'])
-def register():
-    login = request.form.get('login')
-    password = request.form.get('password')
-
-    if db_session.query(User).filter(User.login == login).first():
-        return 'Этот логин уже занят'
-
-    # Создаем нового пользователя
-    user = User(login=login, password=password)
-    db_session.add(user)
-    db_session.commit()
-
-    return redirect(url_for('log'))
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    login = request.form.get('login')
-    password = request.form.get('password')
-
-    user = db_session.query(User).filter(User.login == login, User.password == password).first()
-    if user:
-        session['user_login'] = login
-        return 'Вы успешно вошли'
-    else:
-        return 'Неверный логин или пароль'
+@app.route('/sign_up', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('sign_up.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(email=form.email.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect(f'/user_page/{form.email.data}')
+    return render_template('sign_up.html', title='Регистрация', form=form)
 
 
 @app.route('/')
@@ -58,8 +47,17 @@ def main_page():
 def info(title):
     db_sess = db_session.create_session()
     flow = db_sess.query(Flowers).filter(Flowers.name == title).first()
-    inf = (flow.name, flow.about, flow.mid_price, flow.links, flow.png)
+    inf = (flow.png, flow.name, flow.about, flow.mid_price, flow.links)
+    print(flow.png)
     return render_template('info.html', item=inf)
+
+
+@app.route('/user_page/<email>')
+def upage(email):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.email == email).first()
+    inf = (user.name, user.about, user.email, user.created_date)
+    return render_template('user_page.html', item=inf)
 
 
 def main():
