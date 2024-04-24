f9eaa1a941a0efe98495b1426e44045406167b56
lacozavr <151935499+lacozavr@users.noreply.github.com>
2024-04-24 12:58:27 +0300

    Add files via upload

--- data/flowers.py
+++ data/flowers.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlalchemy
 from data.db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Flowers(SqlAlchemyBase):
@@ -13,3 +14,9 @@ class Flowers(SqlAlchemyBase):
     about = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     png = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     mid_price = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
