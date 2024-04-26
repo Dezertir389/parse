e58882487a90e6415fc1c9b1476ae836acb6b139
lacozavr <151935499+lacozavr@users.noreply.github.com>
2024-04-26 14:16:04 +0300

    Add files via upload

--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
@@ -14,4 +14,5 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
